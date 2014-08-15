4e56488c1953774f0b0423e186b1ac8b88b19c6a
dinghaoyu <835399559@qq.com>
2014-08-15 11:36:32 +0800

    webqq v8.15.am
    选项卡式对话框初步实现

--- webQQ/js/webqq.js
+++ webQQ/js/webqq.js
@@ -4,6 +4,14 @@ $(function(){
 		$(this).addClass("selected");
 	});
 
+	$(".ttList").click(function(){
+		var cid = $(this).attr("cID");
+		$(".ttList").removeClass("cur");
+		$(this).addClass("cur");
+		$(".panBContent").hide();
+		$(".panBContent[cID="+cid+"]").show();
+	});
+
 	$(".cList").click(function(){
 		var fid = $(this).attr("frID");
 		var s =$(this).attr("show");
@@ -20,7 +28,7 @@ $(function(){
 		
 	});
 	$(".pannelChat").draggable({
-		handle:".chatHd",
+		//cancel:".chatHd",
 		containment: ".wrap",
 		scroll: false
 	});
@@ -28,12 +36,74 @@ $(function(){
 		$(".pannelChat").animate({
 		"width":"150px",
 		"height":"30px",
-		"top":"5%"
+		"top":"5%",
+		"left":"0px"
 	},200);
-		$(".pannelChat").append("点我最大化！");
 	});
 	$(".close").click(function(){
 		$(".pannelChat").hide();
+	});
+	
+	$(".ccList").click(function(){
+		var name = $(this).find(".frName").text();
+		var cid = $(this).attr("chatID");
+		var s = $(this).attr("show");
+		//alert(cid);
+		var html1 = "";
+		var html2 = "";
+		html1 += '<div class="nameS" chatID='+cid+'>';
+		html1 += '	<img src="css/images/close.png" class="closeImg">';
+		html1 += '	<span class="naTxt">'+name+'</span>';
+		html1 += '</div>';
+
+		html2 +='<div class="chatBd chat1" chatID='+cid+'></div>';
+		$(".sendBtn").attr("chatID",cid);
+		if (s=="0") {
+			$(".name").append(html1);
+			$(".nameS").removeClass("curName");
+			$(".nameS[chatID="+cid+"]").addClass("curName");
+			$(".chatBd").hide();
+			$(".pannelChat").append(html2);
+			$(this).attr("show","1");
+		};
+	 	if(s=="1"){
+			$(".nameS").removeClass("curName");
+			$(".nameS[chatID="+cid+"]").addClass("curName");
+			$(".chatBd").hide();
+			$(".sendBtn").attr("chatID",cid);
+			$(".chatBd[chatID="+cid+"]").show();
+		};
+		
+	});
+	/*后添加元素的事件委托！！！*/
+	$(document).on("click",".nameS",function(){
+		var c = $(this).attr("chatID");
+		$(".sendBtn").attr("chatID",c);
+		$(".nameS").removeClass("curName");
+		$(this).addClass("curName");
+		$(".chatBd").hide();
+		$(".chatBd[chatID="+c+"]").show();
+	});
+	$(".name").sortable();
+	/*发送消息*/
+	$(".sendBtn").click(function(){
+		var sendMsg = $(this).parent().find(".addTxt").val();
+		var html = "";
+		var cid = $(this).attr("chatID");
+		var name = $(".ccList[chatID="+cid+"]").find(".frName").text();		
+		html += '<div class="self">';
+		html +='	<img src="images/g.jpg">';
+		html +='	<p class="chatNick">我</p>';
+		html +='	<p class="chatContent">'+sendMsg+'</p>';
+		html +='</div>';
+		$(".chatBd[chatID="+cid+"]").append(html);
+		$(".addTxt").val("");
+	});
+	$(document).on("click",".closeImg",function(){
+		var cid = $(this).parent().attr("chatID");
+		$(this).parent().remove();
+		$(".chatBd[chatID="+cid+"]").remove();
+		$(".ccList[chatID="+cid+"]").attr("show","0");
 	});
 	
 })
