afbcba9d8a27316f68f43ca3b113bdd6c8350a93
dinghaoyu <835399559@qq.com>
2014-08-21 11:35:58 +0800

    xiami v8.21.am
    虾米

--- xiami/js/xiami.js
+++ xiami/js/xiami.js
@@ -1,8 +1,51 @@
 $(function(){
-	//setTimeout('loadBG()',1000);
+	// 	setTimeout('loadBG()',1000);
+	/*歌曲列表效果*/
+	$(".songList").hover(function(){
+		$(this).find(".more").show();
+		$(this).find(".dele").show();
+	},function(){
+		$(this).find(".more").hide();
+		$(this).find(".dele").hide();
+	});
+	/*自定义滚动条*/
+	$(".songUL").rollbar({zIndex:80});
+	/*复选框*/
+	$(".checkIn").click(function(){
+		var s=$(this).attr("select");
+		if (s==0) {
+			$(this).css("background-position","-37px -710px");
+			$(this).attr("select","1");
+		};
+		if (s==1) {
+			$(this).css("background-position","");
+			$(this).attr("select","0");
+		};		
+	});
+	$(".checkAll").click(function(){
+		var s=$(this).attr("select");
+		if (s==0) {
+			$(this).css("background-position","-37px -710px");
+			$(".checkIn[select='0']").css("background-position","-37px -710px");
+			$(".checkIn[select='0']").attr("select","1");
+			$(this).attr("select","1");
+		};
+		if (s==1) {
+			$(this).css("background-position","");
+			$(".checkIn[select='1']").css("background-position","");
+			$(".checkIn[select='1']").attr("select","0");
+			$(this).attr("select","0");
+		};
+		
+	});
+});
+
+$(window).load(function(){
+	//loadBG();
 });
 
 function loadBG(){
+	//alert();
 	// stackBlurImage('canvas1', 'canvas', 60, false);
 	var c=document.getElementById("canvas");
 	var ctx=c.getContext("2d");
